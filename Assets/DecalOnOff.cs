ce7c20d93ef034f1c9f38da07179905c72bb1054
Leo-taro FUJIMOTO <lfujimot@e1r12p16.42.fr>
2019-09-23 15:40:04 +0200

    shaders

---
+++ Assets/DecalOnOff.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DecalOnOff : MonoBehaviour
+{
+    Material material;
+    bool showDecal = false;
+
+    void OnMouseDown() {
+        showDecal = !showDecal;
+        if(showDecal) {
+            material.SetFloat("_ShowDecal", 1);
+        } else {
+            material.SetFloat("_ShowDecal", 0);
+        }
+    }
+
+    void Start() {
+        material = this.GetComponent<Renderer>().sharedMaterial;    
+    }
+
+    void Update() {
+
+    }
+}
---
+++ Assets/Transparent.shader
@@ -0,0 +1,16 @@
+﻿Shader "Hollistic/Transparent"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "black" {}
+    }
+    SubShader
+    {
+        Tags { "Queue"="Transparent" }
+        Blend SrcAlpha OneMinusSrcAlpha
+        Cull Off
+        Pass {
+            SetTexture [_MainTex] { combine texture }
+        }
+    }
+}
